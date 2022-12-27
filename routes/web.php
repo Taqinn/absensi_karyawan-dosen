2f799028b5c2eff9f7fbad70326ec164c82eeb64
rjmstor <rianjijim123@gmail.com>
2022-12-27 13:27:53 +0700

    instalasi dan form login

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AbsenController;
+use App\Http\Controllers\AdminController;
+use App\http\Middleware\Role;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/postregister', [AuthController::class, 'postregister']);
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::post('/postlogin', [AuthController::class, 'postlogin']);
+
+Route::get('/home', [AbsenController::class, 'index'])->middleware(['auth', 'role:mahasiswa']);
+Route::post('/absensi', [AbsenController::class, 'absensi'])->middleware(['auth', 'role:mahasiswa']);
+
+
+Route::get('/logout', [AuthController::class, 'logout']);
